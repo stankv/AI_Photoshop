b8778d845d72062d8300016cc3c14b4f3e3fc0ec
stankv <stankv@mail.ru>
2025-12-08 11:35:09 +0300

    add simple buttons

--- src/bot.py
+++ src/bot.py
@@ -10,8 +10,23 @@ async def hello(update, context):
     await send_text(update, context, "Как дела, *дружище*?")
     await send_text(update, context, "Ты написал ..." + update.message.text)
 
+    await send_text_buttons(update, context, "Запустить процесс?", {
+                                             "start": "Запустить",
+                                             "stop": "Остановить",
+    })
+
+async def hello_button(update, context):
+    query = update.callback_query.data
+
+    if query == "start":
+        await send_text(update, context, "Процесс запущен!")
+    else:
+        await send_text(update, context, "Процесс остановлен!")
+
+
 # Создаем Telegram-бота
 app = ApplicationBuilder().token(os.getenv("TELEGRAM_TOKEN")).build()
 # Регистрируем (подключаем) созданные функции
 app.add_handler(MessageHandler(filters.TEXT, hello))
+app.add_handler(CallbackQueryHandler(hello_button))
 app.run_polling()
